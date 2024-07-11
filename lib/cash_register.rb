94ffcffd0e81ec82eb9be45c4ed663362c8a9865
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-11 14:49:01 -0400

    Created CashRegister class with initial values

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+require "pry"
 
+class CashRegister
+
+  attr_accessor :discount, :items, :total
+
+  def initialize(discount = 0)
+    @discount = discount
+    @items = []
+    @total = 0
+
+  end
+
+end
